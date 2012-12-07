16d5e05811a5f98c5328e3ba2a8f57019c556a04
sam9595 <junglesam9595@hotmail.com>
2012-12-07 09:59:17 -0500

    This is just temp, broken version.

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team06/passage/KentonPassageCanditateFinder.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team06/passage/KentonPassageCanditateFinder.java
@@ -1,9 +1,16 @@
 package edu.cmu.lti.oaqa.openqa.test.team06.passage;
 
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +31,8 @@ import java.util.regex.Pattern;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 
 import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.DocumentPreprocessor;
 
 public class KentonPassageCanditateFinder {
   private String text;
@@ -44,11 +53,147 @@ public class KentonPassageCanditateFinder {
     this.textSize = text.length();
     this.scorer = scorer;
   }
+ public int GetLength(List<HasWord> a){
+		int length = 0;
+		for(HasWord token:a){
+			length += token.toString().length();
+		}
+		return length;
+	}
 
+public int Getdifference(TreeMap<Integer,Integer> lengthMap,int key){
+	int diff = 0;
+	for(Map.Entry<Integer,Integer> entry: lengthMap.entrySet()){
+		if(key < entry.getKey())
+			break;
+		diff = entry.getValue();
+	}
+	return diff;
+}
+public String RemoveHTMLtag(String text){
+  	String striptext = text;
+	String htmldelimit = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>";
+    Pattern p = Pattern.compile(htmldelimit);
+    striptext = text.replaceAll(htmldelimit,"");
+    return striptext;
+}
+  public TreeMap<Integer,Integer> MakeLengthMap(String text){
+	  TreeMap<Integer,Integer> lengthMap = new TreeMap<Integer,Integer>();
+	  String htmldelimiter = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>";
+        Pattern p = Pattern.compile(htmldelimiter);
+        Matcher m = p.matcher(text);	
+        int lastend = 0;
+        int cumulength = 0;
+        int cumuHTML = 0;
+        while(m.find()){
+        	cumulength += m.start() - lastend;
+        	cumuHTML += (m.end() - m.start());
+        	lengthMap.put(cumulength, cumuHTML);
+        	lastend = m.end();
+        	System.out.println(m.start() +" "+ m.end());
+        }		  
+        return lengthMap;
+  }
+  /*
+  public List<String> sentencesplitter(String paragraph){
+	  Reader reader = new StringReader(paragraph);
+	  DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+
+	  List<String> sentenceList = new LinkedList<String>();
+	  Iterator<List<HasWord>> it = dp.iterator();
+	  while (it.hasNext()) {
+	     StringBuilder sentenceSb = new StringBuilder();
+	     List<HasWord> sentence = it.next();
+	     for (HasWord token : sentence) {
+	        if(sentenceSb.length()>1) {
+	           sentenceSb.append(" ");
+	        }
+	        sentenceSb.append(token);
+	     }
+	     sentenceList.add(sentenceSb.toString());
+	  }
+	  return sentenceList;
+  }
+  */
+  public int FindEnd(String paragraph, int begin,List<HasWord> a, List<HasWord> b){
+		String ptr = new String(paragraph.substring(begin));
+		int length = begin;
+		
+		for(HasWord token: a){
+			System.out.println("token: " + token + "ptr: " + ptr.substring(0, 100)) ;
+			if(token.toString().length()<3)
+				continue;
+			int len = ptr.indexOf(token.toString());
+			if(len >=0 ){
+				length += (len + token.toString().length());
+				System.out.println("len: " + len + "string length: " + token.toString().length());
+				ptr = ptr.substring(len + token.toString().length());
+			}
+			else{
+				continue;
+			}
+	//		System.out.println("Token: " + token.toString() + " length:" + length);
+		}
+		int len = ptr.indexOf(b.get(0).toString());
+		return length + len;
+} 
+  public List<String> sentencesplitter(String paragraph){
+		Reader reader = new StringReader(paragraph);
+		DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+
+		List<String> sentenceList = new ArrayList<String>();
+		Iterator<List<HasWord>> it = dp.iterator();
+		int len = 0;
+		List<HasWord> sentence;
+		String ptr = new String(paragraph);
+		String nextptr = new String();
+		System.out.println("sentencesplitter");
+		if(it.hasNext()){
+			List<HasWord> next_sentence = it.next();
+			while (it.hasNext()) {
+				StringBuilder sentenceSb = new StringBuilder();
+				sentence = next_sentence;
+				next_sentence = it.next();
+				//System.out.println("nextptr"+nextptr);
+
+				//int begin = ptr.indexOf(sentence.get(0).toString());
+				int begin = 0;
+				int end = FindEnd(ptr,begin,sentence,next_sentence);
+				System.out.println("begin" + begin + "end" + end);
+				//int end = nextptr.indexOf(next_sentence.get(0).toString())+length;
+				System.out.println(ptr.substring(begin, end));
+				sentenceList.add(ptr.substring(begin, end));
+				System.out.println("Get sentences: " + ptr.substring(begin,end));
+				ptr = ptr.substring(end);
+			}
+			sentenceList.add(ptr);
+		}
+		return sentenceList;
+  }
+  
   public List<PassageCandidate> extractPassages(String[] keyterms) {
+	  System.out.println("New Article!!!!!!!!!");
     List<List<PassageSpan>> matchingSpans = new ArrayList<List<PassageSpan>>();
+    String striptext = RemoveHTMLtag(text);
+    TreeMap<Integer,Integer> lengthMap = MakeLengthMap(text);
+    //String striptext = text;
+    String[] sentencestring = striptext.split("[.?!]");
+    List<String > sentences = Arrays.asList(sentencestring);
+//    System.out.println("striptext: ");
+//    System.out.println(striptext);
+//    List<String> sentences = sentencesplitter(striptext);
+//    int len = 0;
+//    System.out.println("total sentences:"+sentences.size());
+//
+//    for(int i = 0 ; i < sentences.size() ;i++){
+//    	System.out.println("Sentence"+i+":   "+sentences.get(i));
+//    	System.out.println("Sentence"+i+":   "+striptext.substring(len, len+sentences.get(i).length()));
+//    	len+=sentences.get(i).length();
+//    }
     
-    String[] sentences = text.split("[.?!]");
+//    System.out.println("OriginalText: ");
+//    System.out.println(striptext.substring(0,len));
+
     
     List<PassageCandidate> result = new ArrayList<PassageCandidate>();
     int accu = 0;
@@ -56,63 +201,53 @@ public class KentonPassageCanditateFinder {
     int sentenceMatches = 0;
     totalMatches = 0;
     totalKeyterms = 0;
-    for(String sent: sentences){
+//    for(String sent: sentences){
+    for(int i = 0 ; i < sentences.size() ;i++){
    // Find all keyterm matches.
+      System.out.println("i::::"+i+"!!!!!!!");
+      String sent = sentences.get(i);
       sentinel = 0;
-      sentenceMatches = 0;
       List<PassageSpan> matchedSpans = new ArrayList<PassageSpan>();
       for (String keyterm : keyterms) {
+    	if(keyterm.length() < 3)
+    		continue;
         Pattern p = Pattern.compile(keyterm);
         Matcher m = p.matcher(sent);
         while (m.find()) {
-          //if(sentinel == 0){
-            //PassageSpan match = new PassageSpan(m.start() + accu, m.end() + accu);
-            PassageSpan match = new PassageSpan(accu + 1, sent.length() + accu + 1);
-            //System.out.println(keyterm + " " + (accu+1) +" "+ (sent.length() + accu + 1));
-            matchedSpans.add(match);
-            
-          //  sentinel = 1;
-          //}
+          if(sentinel == 0){
+        	int begin= 0;
+        	int end = 0;
+        	if(i - 1 >= 0 ){
+        		begin = accu - (sentences.get(i-1).length()+1);
+        	}
+        	else{
+        		begin = 0;
+        	}
+        	if(i+1 < sentences.size()){
+        		end = accu + sent.length() +1+ sentences.get(i+1).length()+1;
+        	}
+        	else{
+        		end = accu + sent.length()+1;
+        	}
+            //PassageSpan match = new PassageSpan(accu , sent.length() + accu + 1);
+        	System.out.println("Begin"+ begin + " end:"+end);
+        	System.out.println("i:"+i+"   "+text.substring(begin,end));
+        	PassageSpan match = new PassageSpan(begin,end);
+            matchedSpans.add(match);          
+            sentinel = 1;
+          }
           totalMatches++;
           sentenceMatches++;
-          continue;
         }
         if (!matchedSpans.isEmpty()) {
           matchingSpans.add(matchedSpans);
           totalKeyterms++;
-        }
-        
-//        if(sentenceMatches != 1){
-//          double score = scorer.scoreWindow(accu + 1, sent.length() + accu + 1,
-//                  sentenceMathces, totalMatches, keytermsFound,
-//                  totalKeyterms, textSize);
-//          PassageCandidate window = null;
-//          try {
-//            window = new PassageCandidate(docId, begin, end, (float) score, null);
-//          } catch (AnalysisEngineProcessException e) {
-//            e.printStackTrace();
-//          }
-//          result.add(window);
-//        }
-        
+        }       
       }
-      accu += (sent.length() + 1);
+      accu += (sent.length()+1);
     }
     System.out.println("TotalMatches:" + totalMatches + "TotalKeyterms" + totalKeyterms);
-//    // Find all keyterm matches.
-//    for (String keyterm : keyterms) {
-//      Pattern p = Pattern.compile(keyterm);
-//      Matcher m = p.matcher(text);
-//      while (m.find()) {
-//        PassageSpan match = new PassageSpan(m.start(), m.end());
-//        matchedSpans.add(match);
-//        totalMatches++;
-//      }
-//      if (!matchedSpans.isEmpty()) {
-//        matchingSpans.add(matchedSpans);
-//        totalKeyterms++;
-//      }
-//    }
+
 
     // create set of left edges and right edges which define possible windows.
     HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
@@ -123,7 +258,6 @@ public class KentonPassageCanditateFinder {
       for (PassageSpan keytermMatch : keytermMatches) {
         Integer leftEdge = keytermMatch.begin;
         Integer rightEdge = keytermMatch.end;
-        //System.out.println(leftEdge + " " + rightEdge);
         hash.put(leftEdge, rightEdge);
        if (!leftEdges.contains(leftEdge)){
           leftEdges.add(leftEdge);
@@ -133,7 +267,7 @@ public class KentonPassageCanditateFinder {
         }
       }
     }
-
+    System.out.println("Hash Map OVER!!!!!!!!!");
     // For every possible window, calculate keyterms found, matches found; score window, and create
     // passage candidate.
 //    List<PassageCandidate> result = new ArrayList<PassageCandidate>();
@@ -163,16 +297,16 @@ public class KentonPassageCanditateFinder {
                 totalKeyterms, textSize);
         PassageCandidate window = null;
         try {
-          System.out.println("Key Sentence: " + text.substring(begin, begin+3));
-          System.out.println("Score: " + score);
-          window = new PassageCandidate(docId, begin, end, (float) score, null);
+        //  System.out.println("Key Sentence: " + text.substring(begin, begin+3));
+        //  System.out.println("Score: " + score);
+          window = new PassageCandidate(docId, begin + Getdifference(lengthMap,begin), end+Getdifference(lengthMap,end), (float) score, null);
         } catch (AnalysisEngineProcessException e) {
           e.printStackTrace();
         }
         result.add(window);
       }
     }
-
+    System.out.println("Find Window OVER!!!!!!!!!");
     // Sort the result in order of decreasing score.
     // Collections.sort ( result , new PassageCandidateComparator() );
     return result;
